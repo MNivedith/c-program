0e781ec6b7567016454500d5e9c3dd2c14ab28d4
MNivedith <46436674+MNivedith@users.noreply.github.com>
2019-01-07 16:10:15 +0530

    Create firstday.c

---
+++ firstday.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int input(void)
+{
+int a;
+printf("enter the number\n");
+scanf("%d",&a);
+return a;
+}
+int compute(int x)
+{
+x=x+6;
+return x;
+}
+void output(int m,int n)
+{
+printf("%d+6 is %d",m,n);
+}
+int main(void)
+{
+int a=input();
+int b=compute(a);
+output(a,b);
+}
