7adbac04b2af09d485dfcb53d87b2f9674cbd016
MNivedith <46436674+MNivedith@users.noreply.github.com>
2019-03-13 17:16:20 +0530

    Create using min variable.c
    #include<stdio.h>
    void input(int *x,int *y,int *z)
    {
     printf("enter the numbers");
     scanf("%d%d%d",x,y,z);
    }
    void compute(int x,int y ,int z,int *min)
    {
     
     if(x<y)
     {
      if(x<z)
      {
       *min=x;
      }
      else
      {
       *min =z;
      }
     }
     if (y<x)
     {
      if(y<z)
      {
       *min=y;
      }
      else 
      {
       *min=z;
      }
     }
    }
    void output(int min)
    {
     printf("min is %d",min);
    }
    int main()
    {
     int x,y,z,min;
     input(&x,&y,&z);
     compute(x,y,z,&min);
     output(min);
     return 0;
    }

---
+++ using min variable.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+void input(int *x,int *y,int *z)
+{
+ printf("enter the numbers");
+ scanf("%d%d%d",x,y,z);
+}
+void compute(int x,int y ,int z,int *min)
+{
+ 
+ if(x<y)
+ {
+  if(x<z)
+  {
+   *min=x;
+  }
+  else
+  {
+   *min =z;
+  }
+ }
+ if (y<x)
+ {
+  if(y<z)
+  {
+   *min=y;
+  }
+  else 
+  {
+   *min=z;
+  }
+ }
+}
+void output(int min)
+{
+ printf("min is %d",min);
+}
+int main()
+{
+ int x,y,z,min;
+ input(&x,&y,&z);
+ compute(x,y,z,&min);
+ output(min);
+ return 0;
+}#include<stdio.h>
+void input(int *x,int *y,int *z)
+{
+ printf("enter the numbers");
+ scanf("%d%d%d",x,y,z);
+}
+void compute(int x,int y ,int z,int *min)
+{
+ 
+ if(x<y)
+ {
+  if(x<z)
+  {
+   *min=x;
+  }
+  else
+  {
+   *min =z;
+  }
+ }
+ if (y<x)
+ {
+  if(y<z)
+  {
+   *min=y;
+  }
+  else 
+  {
+   *min=z;
+  }
+ }
+}
+void output(int min)
+{
+ printf("min is %d",min);
+}
+int main()
+{
+ int x,y,z,min;
+ input(&x,&y,&z);
+ compute(x,y,z,&min);
+ output(min);
+ return 0;
+}
