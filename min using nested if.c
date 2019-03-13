38953b9e88b76c7ce8c5eda68cfe99fd45f89a90
MNivedith <46436674+MNivedith@users.noreply.github.com>
2019-03-13 18:19:24 +0530

    Update min using nested if.c

--- min using nested if.c
+++ min using nested if.c
@@ -35,41 +35,13 @@ else
 	printf("%d is min than %d and %d",z,x,y);
 	}
 }
-}#include<stdio.h>
-int input(int *x,int *y,int *z)
-{
-printf("\n enter three numbers:");
-scanf("%d%d%d",x,y,z);
-return 0;
 }
-int compute(int x,int y,int z,int *min)
+void main()
 {
-*min=x<=y;
-*min=x<z;
-return *min;
+	int x,y,z,min;
+	input(&x,&y,&z);
+	compute(x,y,z,&min);
+	output( x,y,z);
 }
-void output(int x,int y,int z)
-{
-	if(x<=y)
-	{
-		if(x<=z)
-		{
-		printf("%d is min than %d and %d",x,y,z);
-		}
-		else
-		{
-		printf("%d is min than %d and %d",z,x,y);
-		}
-	}	
-else
-{
-	if(y<z)
-	{
-	printf("%d is min than %d and %d",y,x,z);
-	}
-	else
-	{
-	printf("%d is min than %d and %d",z,x,y);
-	}
-}
-}
+
+
