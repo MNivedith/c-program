8fb02b467566dba9654e2c9481021d1c15fa95e4
MNivedith <46436674+MNivedith@users.noreply.github.com>
2019-03-15 16:57:56 +0530

    Create pattern.c

---
+++ lab/pattern.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+int input(int *n)
+{
+ printf("enter the number:");
+ scanf("%d",n);
+ return 0;
+}
+int output(int i,int j,int n)
+{
+ for(i=1;i<=n;i++)
+ {
+   for (j=1;j<=n-i+10;j++)
+    {
+     printf(" ");
+	}
+	for(j=1;j<=i;j++)
+     {
+  		printf(" * ");
+ 	}
+   printf("\n");
+return 0;
+}#include<stdio.h>
+int input(int *n)
+{
+ printf("enter the number:");
+ scanf("%d",n);
+ return 0;
+}
+int output(int i,int j,int n)
+{
+ for(i=1;i<=n;i++)
+ {
+   for (j=1;j<=n-i+10;j++)
+    {
+     printf(" ");
+	}
+	for(j=1;j<=i;j++)
+     {
+  		printf(" * ");
+ 	}
+   printf("\n");
+return 0;
+}
+int main()
+{
+ int n;
+ input(&n);
+ output(n);
+ return 0;
+}#include<stdio.h>
+int input(int *n)
+{
+ printf("enter the number:");
+ scanf("%d",n);
+ return 0;
+}
+int output(int i,int j,int n)
+{
+ for(i=1;i<=n;i++)
+ {
+   for (j=1;j<=n-i+10;j++)
+    {
+     printf(" ");
+	}
+	for(j=1;j<=i;j++)
+     {
+  		printf(" * ");
+ 	}
+   printf("\n");
+return 0;
+}
+int main()
+{
+ int n;
+ input(&n);
+ output(n);
+ return 0;
+}
+int main()
+{
+ int n;
+ input(&n);
+ output(n);
+ return 0;
+}
