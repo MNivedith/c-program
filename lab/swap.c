2b14a5e2b57fa9c883ca8ab16bf17c0e6e71c682
MNivedith <46436674+MNivedith@users.noreply.github.com>
2019-04-11 11:59:40 +0530

    Create swap.c

---
+++ lab/swap.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void input(int* x,int* y)
+{
+ printf("Enter two numbers ");
+ scanf("%d %d",x,y);
+}
+
+void swap(int* x,int* y)
+{
+ int t;
+ t=*x;
+ *x=*y;
+ *y=t;
+}
+void output(int x, int y)
+{
+ printf("After swaping the numbers are %d and %d",x,y);
+}
+
+int main(void)
+{
+ int x,y;
+ input(&x,&y);
+ swap(&x,&y);
+ output(x,y);
+return 0;
+}
